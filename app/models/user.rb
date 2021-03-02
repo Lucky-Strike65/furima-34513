1b9a04b2ce6ac4b7b2cc413f23b8d6a8fee070c7
Lucky-Strike65 <justdoit6210@gmail.com>
2021-03-02 18:49:58 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validatates :nickname
+    validatates :first_name, format: {wiht: /\A[ぁ-んァ-ン一-龥]/,message:"全角ひらがな、全角カタカナ、漢字で入力して下さい"}
+    validatates :last_name,  format: {wiht: /\A[ぁ-んァ-ン一-龥]/,message:"全角ひらがな、全角カタカナ、漢字で入力して下さい"}
+    validatates :birthday
+    validatates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください"}
+    validatates :last_name_kana,  format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください"}
+  end
 end
