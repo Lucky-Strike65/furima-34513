a17ce11ae312d9a89b037038f1b29b6a3b5da7a9
Lucky-Strike65 <justdoit6210@gmail.com>
2021-03-03 13:00:54 +0900

    新規登録、ログインの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validatates :nickname
-    validatates :first_name, format: {wiht: /\A[ぁ-んァ-ン一-龥]/,message:"全角ひらがな、全角カタカナ、漢字で入力して下さい"}
-    validatates :last_name,  format: {wiht: /\A[ぁ-んァ-ン一-龥]/,message:"全角ひらがな、全角カタカナ、漢字で入力して下さい"}
-    validatates :birthday
-    validatates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください"}
-    validatates :last_name_kana,  format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください"}
+    validates :nickname
+    validates :first_name, format:{with: /\A[ぁ-んァ-ン一-龥]/,message:"全角ひらがな、全角カタカナ、漢字で入力して下さい"}
+    validates :last_name,  format:{with: /\A[ぁ-んァ-ン一-龥]/,message:"全角ひらがな、全角カタカナ、漢字で入力して下さい"}
+    validates :birthday
+    validates :first_name_kana, format:{with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください"}
+    validates :last_name_kana,  format:{with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください"}
+    validates :password,format:{with: /\A[a-z0-9]+\z/i,message: "半角英数字で入力して下さい"}
   end
 end
