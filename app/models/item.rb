9a9493df4ff5026889dd6215b3584bd2332931f0
Lucky-Strike65 <justdoit6210@gmail.com>
2021-03-04 21:55:27 +0900

    AcitveHash導入設定、バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :order
   has__one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category,     numericality: { other_than: 1 }
+  belongs_to :condition,    numericality: { other_than: 1 }
+  belongs_to :sorce,        numericality: { other_than: 1 }
+  belongs_to :burden,       numericality: { other_than: 1 }
+  belongs_to :days_to_ship, numericality: { other_than: 1 }
 
+  with_options presence: ture do
+    validates :name
+    validates :price
+    validates :category_id
+    validates :burden_id
+    validates :source_id
+    validates :days_to_ship_id
+    validates :item_description
+  end
 end
