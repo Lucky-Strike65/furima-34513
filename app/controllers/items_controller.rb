e223ac63f1f39bcbcdb5fcfbf65bcc58c774ea53
Lucky-Strike65 <justdoit6210@gmail.com>
2021-03-04 14:49:05 +0900

    ルーティング設定、コントローラー設定、ログイン時での商品出品ページへの遷移。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,16 @@
 class ItemsController < ApplicationController
+
   def index
   end
-  
+
+  def new
+    if current_user
+      @item = Item.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+  end
 end
