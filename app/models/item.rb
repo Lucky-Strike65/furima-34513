95939034f7993b76620e6212065b17cc0d7a95fc
Lucky-Strike65 <justdoit6210@gmail.com>
2021-03-05 18:03:14 +0900

    バリデーション関連をリファクタリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,12 +12,14 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :price,            numericality: { other_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-    validates :category_id,      numericality: { other_than: 1 }
-    validates :burden_id,        numericality: { other_than: 1 }
-    validates :source_id,        numericality: { other_than: 0 }
-    validates :days_to_ship_id,  numericality: { other_than: 1 }
     validates :item_description
-    validates :condition_id,        numericality: { other_than: 1 }
+    validates :price, numericality: { other_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} do
+    with_options numericality: {other_than: 0} do
+      validates :category_id
+      validates :burden_id
+      validates :source_id
+      validates :days_to_ship_id
+      validates :condition_id
+    end
   end
 end
