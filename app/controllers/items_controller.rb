549421a5d18722370e7f44cee572d22a00d0232b
Lucky-Strike65 <justdoit6210@gmail.com>
2021-03-06 20:12:53 +0900

    itemコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:price,:category_id,:condition_id,:burden_id,:source_id,:days_to_ship_id,:item_description).merge(user_id: current_user.id)
+    params.require(:item).permit(:name,:price,:category_id,:condition_id,:burden_id,:source_id,:days_to_ship_id,:item_description,:image).merge(user_id: current_user.id)
   end
 end
