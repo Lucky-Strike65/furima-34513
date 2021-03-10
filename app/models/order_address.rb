3ff2b9f3b6e110b3a1762bafe89fe53ff62d5c23
Lucky-Strike65 <justdoit6210@gmail.com>
2021-03-10 13:35:08 +0900

    Formオブジェクト作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,14 @@
+class OrderAddress
+  include ActiveModel::ActiveModel
+  attr_accessor :item_id,:user_id, :order_id, :postcode, :city, :building,:phone_number, :prefecture_id, :area
+
+  with_options presence: true do
+    validates :item_id
+    validates :order_id
+    validates :postcode
+    validates :city
+    validates :phone_number
+    validates :prefecture_id
+    validates :area
+  end
+end
