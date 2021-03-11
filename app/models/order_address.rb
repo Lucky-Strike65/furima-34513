7cdafbc8e8435b21d2d52e1c8565fa2388b6c99a
Lucky-Strike65 <justdoit6210@gmail.com>
2021-03-11 18:33:29 +0900

    order_addressモデルのバリデーション設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,16 +4,14 @@ class OrderAddress
 
   with_options presence: true do
     validates :item_id
-    validates :order_id
-    validates :postcode, format{with: 
+    validates :postcode, format:{with: /\d{3}-\d{4}/} 
     validates :city
-    validates :phone_number, format{with: }
+    validates :phone_number,format:{with: /\A\d{11}\z/}
     validates :prefecture_id, numericality:{other_than: 0 }
     validates :area
   end
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    binding.pry
     Address.create(postcode: postcode, city: city, building: building, phone_number: phone_number, prefecture_id: prefecture_id, area: area, order_id: order.id)
   end
 
