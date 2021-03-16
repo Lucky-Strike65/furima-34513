d7f4d4833e2a333d1297d874630c4954d62d1cd2
Lucky-Strike65 <justdoit6210@gmail.com>
2021-03-16 16:42:00 +0900

    JavaScriptによるpreview機能実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,3 +1,30 @@
-document.addEventListener('DOMContentLoaded',function(){
-  console.log(123)
-});
+if (document.URL.match( /new/ ) || document.URL.match( /edit/)){
+  document.addEventListener('DOMContentLoaded',function(){
+    const ImageList = document.getElementById('image-list');
+
+
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.id= "preview"
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('item-image').addEventListener('change',function(e){
+
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
+    });
+  });
+}
